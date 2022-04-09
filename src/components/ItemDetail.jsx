c663c5bfe2d8d07d46a326159a8cdb945408c296
Daniel Quintero <daniel.manny10@hotmail.com>
2022-04-08 20:35:39 -0500

    add Loader

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 
-const ItemDetail = ({prd}) => {
-  return (
-    <>
-        <Card className='space' key={prd.id} style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={prd.image} />
-        <Card.Body className='d-flex flex-column'>
-          <Card.Title>{prd.name}</Card.Title>
-          <Card.Text>
-            {prd.description}
-          </Card.Text>
-          <Card.Text>
-            $ {prd.price}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </>
-  )
+const ItemDetail = ({ prd }) => {
+    return (
+        <>
+            <Card className='space' key={prd.id} style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={prd.image} />
+                <Card.Body className='d-flex flex-column'>
+                    <Card.Title>{prd.name}</Card.Title>
+                    <Card.Text>
+                        {prd.description}
+                    </Card.Text>
+                    <Card.Text>
+                        $ {prd.price}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>
+    )
 }
 
 export default ItemDetail
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,22 +1,39 @@
 import React, { useEffect } from "react"
 import { useState } from "react"
+import { Spinner } from "react-bootstrap";
 import CustomFetch from "../data/CustomFetch";
 import products from "../data/data";
 import ItemList from "./ItemList";
 
 export default function ItemListContainer() {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
   useEffect(() => {
     CustomFetch(2000, products)
       .then(result => setItems(result))
-      .catch(error => console.log("error"));
+      .catch(error => console.log("error"))
+      .then(() => setIsLoading(false));
   }, [items])
 
   return (
 
     <>
       <div className="wrap d-flex justify-content-around mt-2">
-        <ItemList prod={items} ></ItemList>
+        { 
+          isLoading ? 
+          <div className="colum">
+            <div className="loaderContainer" >
+          <Spinner animation="grow" variant="primary" className="spinner-grow-lat" /> 
+          <Spinner className="spinner-grow-Center" animation="grow" variant="secondary"/>
+          <Spinner animation="grow" variant="primary"className="spinner-grow-lat" />
+          </div>
+            <h4 className="loadingProducts">Loading...</h4>
+          </div>
+          : 
+          <ItemList prod={items} />
+        }
+        
       </div>
     </>
   );
